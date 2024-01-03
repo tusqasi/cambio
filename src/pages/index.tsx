b83d7a8f38e8826f5d15cfb6a149d763cbbef1f3
tusqasi <2002.tushar.kuntawar@gmail.com>
2024-01-04 02:46:04 +0530

    Done?+1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { Toaster, toast } from "sonner";
 import { useRef } from "react";
 import { MutableRefObject } from "react";
+import Head from "next/head";
 
 function uploadImage(file: any, setScaledUrl: Function, scale: number, ref: MutableRefObject<HTMLDivElement>) {
 	const formData = new FormData();
@@ -34,7 +35,7 @@ function uploadImage(file: any, setScaledUrl: Function, scale: number, ref: Muta
 			let image = new Blob([byteArray], { type: data.data.type });
 			setScaledUrl(URL.createObjectURL(image));
 			// toast.success("Resized image")
-			ref.current.scrollIntoView({block:"start",  behavior: "smooth" });
+			ref.current.scrollIntoView({ block: "start", behavior: "smooth" });
 		})
 		.catch((err) => console.error(err));
 }
@@ -57,6 +58,12 @@ export default function Component() {
 	return (
 		<>
 			<Toaster />
+			<Head>
+				<meta property="og:title" content="Cambio - An image resize utility" />
+				<meta property="og:description" content="Cambio - An image resize utility made in nextjs" />
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://cambio-eight.vercel.app/" />
+			</Head>
 			<div className=" h-[100svh] flex flex-col justify-between  ">
 				<header className="flex justify-center py-6 bg-[#ffffff]">
 					<h1 className="text-4xl font-bold  ">Cambio</h1>
@@ -180,11 +187,29 @@ export default function Component() {
 					</div>
 				</main >
 				<footer className=" flex justify-center gap-6 p-6 bg-black">
-					<Link href="#">
+					<Link
+						aria-label="LinkedIn"
+						href="https://www.linkedin.com/in/tushar-kuntawar-9a7b8923b/"
+						target="_blank"
+						className=" transition-transform  scale-100 hover:scale-110  "
+					>
+						<LinkedinIcon className="w-4 h-4 text-white" />
+					</Link>
+					<Link
+						aria-label="Twitter"
+						href="https://twitter.com/tusharkuntawar"
+						target="_blank"
+						className=" transition-transform  scale-100 hover:scale-110  "
+					>
 						<TwitterIcon className="w-4 h-4 text-white" />
 					</Link>
-					<Link href="#">
-						<LinkedinIcon className="w-4 h-4 text-white" />
+					<Link
+						aria-label="GitHub"
+						href="https://github.com/tusqasi/"
+						target="_blank"
+						className=" transition-transform  scale-100 hover:scale-110  "
+					>
+						<GithubIcon className="w-4 h-4 text-white" />
 					</Link>
 				</footer>
 			</div >
@@ -235,3 +260,22 @@ function TwitterIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
 	)
 }
 
+function GithubIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			{...props}
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+			<path d="M9 18c-4.51 2-5-2-7-2" />
+		</svg>
+	);
+}
