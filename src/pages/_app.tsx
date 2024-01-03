c16c3a67470405303215ce2013f23511e19472a0
tusqasi <2002.tushar.kuntawar@gmail.com>
2024-01-03 23:16:51 +0530

    Fix firefox insecure content error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,12 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { GeistSans } from 'geist/font/sans';
 
+
 export default function App({ Component, pageProps }: AppProps) {
-	return (<main className={GeistSans.className}>
-		<Component {...pageProps} />
-	</main>);
+	return (
+		<main className={GeistSans.className}>
+			<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+			<Component {...pageProps} />
+		</main>
+	);
 }
